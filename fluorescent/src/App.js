28c43a6c379a927a855eb7c48d4160922d9ad9c7
VishnudevP <vipoil04@gmail.com>
2023-01-25 12:05:05 -0500

    testing

--- fluorescent/src/App.js
+++ fluorescent/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> if this changes then git works
         </p>
         <a
           className="App-link"
